68b69043b9a3e141ea1045527354d845715dcf43
sumondas2001 <sumonchandradas1010@gmail.com>
2024-09-20 03:00:44 +0600

    coffee details section add

--- index.js
+++ index.js
@@ -54,6 +54,13 @@ async function run() {
                const query = { _id: new ObjectId(id) };
                const result = await coffeeCollection.findOne(query);
                res.send(result)
+          });
+          app.get('/coffeeDetails/:id', async (req, res) => {
+               const id = req.params.id;
+               const query = { _id: new ObjectId(id) };
+               const result = await coffeeCollection.findOne(query);
+               res.send(result);
+
           })
 
           app.delete('/coffee/:id', async (req, res) => {
